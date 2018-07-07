39f641ad68695a8eddcc3d484de8b4478a3e63a3
Robiullah2244 <robiullah2244@gmail.com>
2018-07-08 04:05:12 +0600

    ejs

--- Node.js/App.js
+++ Node.js/App.js
@@ -11,10 +11,43 @@ app.set('view engine','ejs');
 
 //MIDDLEWARES
 app.use(bodyParser.urlencoded({extended:false}));
+app.use('/Contents',express.static('Contents'));
 
 // ROUTES
-app.use('/login', login);
+//app.use('/login', login);
+app.get('/',function(req,res)
+{
+	res.render('publicHome');
+})
+app.get('/home',function(req,res)
+{
+	res.render('home');
+})
+app.get('/publicHome',function(req,res)
+{
+	res.render('publicHome');
+})
 
+app.get('/HouseDetails',function(req,res)
+{
+	res.render('HouseDetails');
+})
+app.get('/UserDashBoard',function(req,res)
+{
+	res.render('UserDashBoard');
+})
+app.get('/userAllPosts',function(req,res)
+{
+	res.render('userAllPosts');
+})
+app.get('/userPosts',function(req,res)
+{
+	res.render('userPosts');
+})
+app.get('/userProfile',function(req,res)
+{
+	res.render('userProfile');
+})
 //SERVER START
 app.listen(port,function() {
   console.log('Server started');
