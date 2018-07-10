c659f8819d648bae4bb7f5e44e4bf2688470b798
Robiullah2244 <robiullah2244@gmail.com>
2018-07-11 03:40:20 +0600

    merged

--- Node.js/Controllers/HouseDetails.js
+++ Node.js/Controllers/HouseDetails.js
@@ -15,13 +15,9 @@ router.get('/:id', function(req, res){
 		var signedInUser = req.session.userName;
 		if(typeof signedInUser != 'undefined')
 		{
-<<<<<<< HEAD
 			console.log("signedInUser: "+signedInUser+"   u: "+result[0].UserName);
 			res.render('HouseDetails',{signedInUser: signedInUser, result : result});
-=======
-			res.render('HouseDetails',{result : result, signedInUser: signedInUser});
->>>>>>> 48511c2152569afd9ed4dae3a71c44175b2f6add
-		}
+
 		else
 		{
 			res.render('PublicHouseDetails',{result : result});
