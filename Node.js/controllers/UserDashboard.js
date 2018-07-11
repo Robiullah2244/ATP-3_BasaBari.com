84508ca523bdb72d94c172f2dead049c7c2678b4
IKTANIM <iktanim1995@gmail.com>
2018-07-11 12:13:23 +0600

    User Dashboard resubmit

--- Node.js/controllers/UserDashboard.js
+++ Node.js/controllers/UserDashboard.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require.main.require('./models/user-model2');
+  var arr=[];
+  var i=0;
 router.get('/', function(req, res){
   // res.render('SignUp',{SignUpData:[],errs:[]});
     var userName ='tanim';//req.session.userName;
-  userModel.getNumberOfPostByUserName(userName,function(result){
-  	res.render('UserDashBoard',{signedInUser : req.session.userName, numberOfPost:result[0].numberOfPost});
+    var popularLocation=["Banani","Bashundhar","Nikunjo","Dhanmondi","Glushan","Uttara","Khilgaon","Mirpur","Farmgate"]
+    // console.log(req.session.userName+"Session userName");
+  // userModel.getNumberOfPostByUserName(userName,function(result){
+  // 	res.render('UserDashBoard',{n:m,signedInUser: userName, numberOfPost:result[0].numberOfPost});
+  // });
+
+  popularLocation.forEach (function(item) {
+    userModel.getNumberOfPostByLocation(item, function(result) {
+      // console.log(popularLocation[i]);
+      arr[i++] = result[0].NumberOfPostByLocation;
+
+    })
   });
+    userModel.getNumberOfPostByUserName(userName,function(result){
+    	res.render('UserDashBoard',{arr,signedInUser: userName, numberOfPost:result[0].numberOfPost});
+    });
+
 });
 module.exports=router;
